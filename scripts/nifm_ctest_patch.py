f753eb0a0459edb5cad0f3d0b4b173c59a1d1d63
borntohonk <6264306+borntohonk@users.noreply.github.com>
2023-05-31 18:28:55 +0200

    Update nifm patch described

--- scripts/nifm_ctest_patch.py
+++ scripts/nifm_ctest_patch.py
@@ -7,8 +7,8 @@ def get_build_id():
 
 with open('uncompressed_nifm.nso0', 'rb') as fi:
     read_data = fi.read()
-    result = re.search(b'.{16}\xf5\x03\x01\xaa\xf4\x03\x00\xaa.{4}\xf3\x03\x14\xaa\xe0\x03\x14\xaa\x9f\x02\x01\x39\x7f\x8e\x04\xf8', read_data)
-    patch = '%06X%s%s' % (result.start(), '0008', 'E0031FAAC0035FD6')
+    result = re.search(b'.{20}\xf4\x03\x00\xaa.{4}\xf3\x03\x14\xaa\xe0\x03\x14\xaa\x9f\x02\x01\x39\x7f\x8e\x04\xf8', read_data)
+    patch = '%06X%s%s' % (result.start(), '0014', '00309AD2001EA1F2610100D4E0031FAAC0035FD6')
     text_file = open(get_build_id() + '.ips', 'wb')
     print('nifm build-id: ' + get_build_id())
     print('nifm_ctest offset and patch at: ' + patch)
