a5ca3ede520ab92a4f98e9e67b1a86314c742d0a
borntohonk <6264306+borntohonk@users.noreply.github.com>
2023-05-31 18:38:25 +0200

    Update from JDK11 to JDK17, Update ghidra references

--- scripts/setup_ghidra.sh
+++ scripts/setup_ghidra.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd $HOME/ && \
-wget https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.15%2B10/OpenJDK11U-jdk_x64_linux_hotspot_11.0.15_10.tar.gz && \
-mkdir ojdk11 && \
-tar xvzf OpenJDK11U-jdk_x64_linux_hotspot_11.0.15_10.tar.gz -C ojdk11 && \
-rm OpenJDK11U-jdk_x64_linux_hotspot_11.0.15_10.tar.gz && \
-export JAVA_HOME=$HOME/ojdk11/jdk-11.0.15+10 && \
-export PATH="$HOME/ojdk11/jdk-11.0.15+10/bin:$PATH" && \
-echo "export PATH="$HOME/ojdk11/jdk-11.0.12+7/bin:$PATH"" >> $HOME/.profile && \
+wget https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.7%2B7/OpenJDK17U-jdk_x64_linux_hotspot_17.0.7_7.tar.gz && \
+mkdir ojdk17 && \
+tar xvzf OpenJDK17U-jdk_x64_linux_hotspot_17.0.7_7.tar.gz -C ojdk17 && \
+rm OpenJDK17U-jdk_x64_linux_hotspot_17.0.7_7.tar.gz && \
+export JAVA_HOME=$HOME/ojdk17/jdk-17.0.7+7 && \
+export PATH="$HOME/ojdk17/jdk-17.0.7+7 /bin:$PATH" && \
+echo "export PATH="$HOME/ojdk17/jdk-17.0.7+7 /bin:$PATH"" >> $HOME/.profile && \
 wget $(curl -s https://api.github.com/repos/NationalSecurityAgency/ghidra/releases/latest | grep "browser_download_url" | cut -d '"' -f 4) -O ghidra.zip && \
 unzip ghidra.zip -d ghidra && \
 rm ghidra.zip && \
